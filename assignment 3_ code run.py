0058146e4c64b50a92776bd1a554f8024061fbe1
KARTIKMITTA <98331174+KARTIKMITTA@users.noreply.github.com>
2022-02-14 04:21:59 -0800

    Add files via upload

---
+++ assignment 3_ code run.py
@@ -0,0 +1,76 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======================================================= RESTART: C:/Users/asus/AppData/Local/Programs/Python/Python310/code _2.py ======================================================
+question 1
+ENTER ANY STRING :python programm
+{'python': 1, 'programm': 1}
+   
+
+Question 2
+ENTER THE DAY:
+============================================================= RESTART: C:/Users/asus/Desktop/assignment 3 _ course code.py =============================================================
+Please enter your own String : python programe
+Please enter your own Character : p
+The total Number of Times  p  has Occurred =  2
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+Question 2
+ENTER THE DAY:12
+Enter the month:02
+Enter a year between 1800 and 2025:2022
+13 / 2 / 2022
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+The list is
+[3, 9, 10]
+The resultant tuple is :
+[(3, 9), (9, 81), (10, 100)]
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+enter grade of student:
+9
+your grade is 'A' and excellent performance
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+Enter your sid: 
+21103100
+Enter name: 
+kartik mittal
+Do you want to enter another student entry(Y or N):Y
+Enter sid: 21103091
+Enter name: SARTHAK MAHESHWARI
+Do you want to enter another student entry(Y or N):N
+<a>
+ {21103100: 'kartik mittal', 21103091: 'SARTHAK MAHESHWARI'}
+<b>
+ {21103091: 'SARTHAK MAHESHWARI', 21103100: 'kartik mittal'}
+<c>
+ {21103091: 'SARTHAK MAHESHWARI', 21103100: 'kartik mittal'}
+Search Name with SID: 
+21103100
+<d>
+ kartik mittal
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+ENTER THE NUMBER OF TERMS IN THE SERIES:  
+5
+Fibonacci sequenceL: 
+0
+1
+1
+2
+3
+AVERAGE OF THE RESULTANT FIBONACCI SERIES IS:  1.4
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+<a>
+ {1, 3, 5, 6, 8}
+<b>
+ {3, 6, 8, 9, 13, 17}
+<c>
+ {1, 2, 4, 5}
+<d> 
+ {6, 7, 8, 9, 10}
+<e>
+ {10}
